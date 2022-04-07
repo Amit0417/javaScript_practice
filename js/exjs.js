753a5409758f82022f35b77e3147982b3f904d44
AMIT KUMAR MAHOUR <r.amit.mahour@gmail.com>
2022-04-07 13:53:09 +0530

    fisrt practice

---
+++ js/exjs.js
@@ -0,0 +1,8 @@
+
+function myFunction(){
+    document.getElementById('jfunc').innerHTML = 'Paragraph Changed';
+}
+
+function myFunction1(){
+    document.getElementById('jfunc').innerHTML = 'A Paragraph';
+}
