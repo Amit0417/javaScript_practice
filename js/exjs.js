a28a273050268421f009a1f0d1b8aefe4de62360
AMIT KUMAR MAHOUR <r.amit.mahour@gmail.com>
2022-04-08 07:55:46 +0530

    update

--- js/exjs.js
+++ js/exjs.js
@@ -1,8 +1,12 @@
 
 function myFunction(){
+    alert('Are you sure you want to change paragraph?')
     document.getElementById('jfunc').innerHTML = 'Paragraph Changed';
+   
 }
 
 function myFunction1(){
+    alert('Are you sure you want to revert paragraph?')
     document.getElementById('jfunc').innerHTML = 'A Paragraph';
+   
 }
